7bcf97aad880dec501207fe8d5541ba160b871c7
Pronoy Banik <pronoybanik82@gmail.com>
2025-07-02 15:22:17 +0600

    chnage url

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
-app.use(cors({ origin: ["https://critiqo-frontend-project.vercel.app",], credentials: true }));
+app.use(cors({ origin: ["https://critiqo-frontend-project-n294.vercel.app",], credentials: true }));
 app.use(cors({ origin: ["http://localhost:3000",], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
